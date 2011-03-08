33c1dd7643881c611a23e5c0cc0a5da673e8109a
Norman Clarke <norman@njclarke.com>
2011-03-08 12:20:23 -0300

    Bumped version.

--- lib/prequel/version.rb
+++ lib/prequel/version.rb
@@ -1,8 +1,8 @@
 module Prequel
   module Version
     MAJOR  = 0
-    MINOR  = 0
-    TINY   = 3
+    MINOR  = 1
+    TINY   = 0
     BUILD  = nil
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
